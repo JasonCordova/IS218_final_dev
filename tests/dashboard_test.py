bd21f8f972cbbc1ceb10b608b8b202e7e8a2c768
Jason Cordova <jasoncordova.edu@gmail.com>
2022-05-10 21:54:45 -0400

    TEST #11: Test Dashboard Access Successful

---
+++ tests/dashboard_test.py
@@ -0,0 +1,23 @@
+from app import db
+from app.db.models import User
+
+from flask_login import FlaskLoginClient
+from app.auth.forms import *
+
+def test_dashboard_accessed(application):
+
+    application.test_client_class = FlaskLoginClient # Sets a fake testing client
+
+    user = User('keith@webizly.com', 'testtest')
+    db.session.add(user)
+    db.session.commit() # What we did so far was just make keith@webizly a fake user as done from auth_test
+
+    assert user.email == 'keith@webizly.com'
+    assert db.session.query(User).count() == 1 # Make sure keith@webizly.com is the only user.
+
+    with application.test_client(user=user) as client:
+
+        response = client.get('/dashboard')
+        assert b'keith@webizly.com' in response.data
+        assert response.status_code == 200 # Successful access to dashboard as user keith@webizly.com
+
