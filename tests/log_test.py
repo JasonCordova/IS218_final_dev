493175f4e2d8403b0197b8085876454736b643d4
Jason Cordova <jasoncordova.edu@gmail.com>
2022-05-11 10:29:00 -0400

    TEST #27: Test Werkzeug Log

--- tests/log_test.py
+++ tests/log_test.py
@@ -29,7 +29,12 @@ def test_myapp_log():
 
 def test_werkzeug_log():
 
-    assert True
+    root = os.path.dirname(os.path.abspath(__file__))
+    requestlog = os.path.join(root, '../logs/werkzeug.log')
+
+    if not os.path.exists(requestlog):
+        os.mknod(requestlog)
+    assert os.path.exists(requestlog) == True
 
 def test_sqlalchemy_log():
 
