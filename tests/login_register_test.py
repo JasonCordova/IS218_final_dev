7dc418a2893abfd392e99c13e06fde3b024c221e
Jason Cordova <jasoncordova.edu@gmail.com>
2022-05-11 00:19:13 -0400

    TEST #13: Successful Login Test

---
+++ tests/login_register_test.py
@@ -0,0 +1,20 @@
+from app.auth.forms import login_form, register_form
+from faker import Faker
+
+from wtforms import Form as WTForm
+from flask_wtf import Form as FlaskForm
+from app.auth import login
+from flask_login import current_user
+from flask_login import FlaskLoginClient
+from tests import conftest
+from app import create_app, User
+
+def test_user_login(application):
+
+    with application.test_request_context():
+
+        form = login_form()
+        form.email.data = "FAKEUSER@gmail.com"
+        form.password.data = "testtest"
+        form.submit
+        assert form.validate
